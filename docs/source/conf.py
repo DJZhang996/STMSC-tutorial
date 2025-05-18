ab71baa168c3f6c39c71426d17a71da61ab56b00
DJZhang996 <zzdj96@gmail.com>
2025-05-18 21:23:38 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'STMSC'
+copyright = '2024, Daijun Zhang'
+author = 'Daijun Zhang'
 
 release = '0.1'
 version = '0.1.0'
